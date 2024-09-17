59100b50dcfaaa1af37ecf9b0a582487584c8d53
SU <gfhhjfhgfg4@mail.ru>
2024-09-17 18:13:32 +0300

    добавлен класс наследник

--- heroClasses/heroesClasses.py
+++ heroClasses/heroesClasses.py
@@ -7,33 +7,38 @@ class Hero:
     def __init__(self, name, character_class):
         """Написан конструктор для класса"""
         self.__name = name
-        self.__charter_class = character_class
+        self.__character_class = character_class
         self.__my_hero_skills = []
         self.__level = 0
         self.__exp = 0
 
     def name(self):
+        """"геттер для имени"""
         return self.__name
 
     def character_class(self):
-        return self.__charter_class
+        """"геттер для класса"""
+        return self.__character_class
 
     def my_hero_skills(self):
+        """"геттер для навыков"""
         return self.__my_hero_skills
 
     def level(self):
+        """"геттер для уровня"""
         return self.__level
 
     def exp(self):
+        """"геттер для опыта"""
         return self.__exp
 
-    def get_skills(self, character_class):
+    def get_skills(self):
         """Добавлен геттер для навыков, результат зависит от выбранного класса"""
-        if character_class == 'воин':
+        if self.__character_class == 'воин':
             return self.__warrior_skills
-        elif character_class == 'маг':
+        elif self.__character_class == 'маг':
             return self.__mage_skills
-        elif character_class == 'рейнджер':
+        elif self.__character_class == 'рейнджер':
             return self.__ranger_skills
         else:
             exit("Ошибка перезапустите программу!")
@@ -50,10 +55,11 @@ class Hero:
             self.add_skill()
         else:
             self.__level = 0
-        return f"Герой {self.name}, теперь {self.level} уровня, навыки: {', '.join(self.my_hero_skills)}"
+        return f"Герой {self.name}, теперь {self.level} уровня, навыки: {', '.join(self.__my_hero_skills)}"
 
     def add_exp(self, exp):
-        self.exp += exp
+        """метод для добавления опыта"""
+        self.__exp += exp
         new_level = self.get_new_level()
         return new_level
 
@@ -62,4 +68,52 @@ class Hero:
 
 
 class MyHero(Hero):
-    pass
+
+ """"класс наследник"""
+
+def __init__(self, name, character_class):
+    """"метод инициализации"""
+    super().__init__(name, character_class)
+    self.skill_list = self.skill_list()
+
+
+def add_skill(self):
+    """"метод для добавления навыка"""
+    for _ in range(self.get_new_level()):
+        while True:
+            print(f"доступные навыки: {", ".join(self.skill_list)}")
+            new_skill = input("выберите навык: ")
+            if new_skill in self.skill_list:
+                self.__my_hero_skills.append(new_skill)
+                self.skill_list.remove(new_skill)
+                break
+            else:
+                print("такого нет навыка в списке данных попробуйте снова")
+        if self.get_level() == len(self.__my_hero_skills):
+            break
+    return f"добавлены навыки: {", ".join(self.get_my_hero_skills())}"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
